a777eb67f98acddca85c8e448f5493a275f2c585
Nitin-web-dev <nitinwebdev5@gmail.com>
2026-07-09 18:45:47 +0530

    added basic api for testing

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const dotenv = require("dotenv");
 
 
 const app = express();
+app.use(cors());
+app.use(express.json()); // This lets Express read JSON sent by the client.
+
+// basic api
+app.get('/', (req,res) => {
+    res.send({id: 1, message: "hello word"});
+})
 
 
 const PORT = process.env.PORT || 5000;
