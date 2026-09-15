6e7667610a9e70d1e038537a81e7cae4f8148eac
Nitin-web-dev <nitinwebdev5@gmail.com>
2026-09-15 15:35:13 +0530

    added some new structure api,store,pages,component=> axios,zustand,react-router-dom,reat-hook-form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,13 @@
 import { useState ,useEffect } from 'react'
-import Login from './pages/Login';
+
 
 
 function App() {
-const [tasks, setTasks] = useState([]);
 
-// console.log(tasks);
-useEffect(() => {
-  async function fetchtask() {
-    try {
-      const response = await fetch ("http://localhost:5000/");
-      const data = await response.json();
-      setTasks(data);
-    }catch(error){
-      console.log(error);
-    }
-  }
-
-  // fetchtask();
-},[]);
 
   return (
     <>
-      <Login/>
+        the new foundation of mine fullstack journey
     </>
   )
 }
